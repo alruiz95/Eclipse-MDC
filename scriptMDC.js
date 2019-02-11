54697bf4be115fade41a280da67177c4ceb1d116
pertinaxRP <alruiz300795@gmail.com>
2019-02-10 22:13:05 -0600

    Search done

--- scriptMDC.js
+++ scriptMDC.js
@@ -1,19 +1,30 @@
 var darkThemeClass = "blackTheme";
 var whiteThemeClass = "whiteTheme";
 
+var darkThemeClassMenu = "darkBackground";
+var whiteThemeClassMenu = "whiteBackground";
+
 var CompleteMDCid="CompleteMDC";
+var lefrMenuId="leftMenuID";
 var IdDarkCheckBox = "darkCheckBox";
-var IdInnerMDC = "InnerContent";
+var IdInnerMDC = "mdcContentMain";
+
+var PersonContent = "personContentID";
+var VehicleContent = "vehicleContentID";
 
 function onLoad(){
     var checked = localStorage.getItem('darkTheme');
     if ((checked === 'true') || checked == null){
         document.getElementById(CompleteMDCid).classList.add(darkThemeClass);
         document.getElementById(CompleteMDCid).classList.remove(whiteThemeClass);
+        document.getElementById(lefrMenuId).classList.add(darkThemeClassMenu);
+        document.getElementById(lefrMenuId).classList.remove(whiteThemeClassMenu);
         document.getElementById(IdDarkCheckBox).checked = true;
     } else {
         document.getElementById(CompleteMDCid).classList.remove(darkThemeClass);
         document.getElementById(CompleteMDCid).classList.add(whiteThemeClass);
+        document.getElementById(lefrMenuId).classList.remove(darkThemeClassMenu);
+        document.getElementById(lefrMenuId).classList.add(whiteThemeClassMenu);
         document.getElementById(IdDarkCheckBox).checked = false;
         localStorage.setItem('darkTheme', false);
     }
@@ -21,29 +32,48 @@ function onLoad(){
     dragElement(document.getElementById("CompleteMDC"),document.getElementById("mainHeader"));
     jQuery("#LSPDHead").fitText();
     jQuery("#MDCHead").fitText();
-    clickMenuOption("home");
+    clickMenuOption("search");
 }
 
 function clickMenuOption(option){
-  $("#"+IdInnerMDC).load("include/" + option +".html"); 
-  //console.log(str);
-  //document.getElementById(IdInnerMDC).innerHTML(str);
+  console.log(option);
+
+  var txtFile = new XMLHttpRequest();
+  txtFile.open("GET", option + ".html", true);
+  txtFile.onreadystatechange = function() {
+    if (txtFile.readyState === 4) {  // Makes sure the document is ready to parse.
+      if (txtFile.status === 200) {  // Makes sure it's found the file.
+        allText = txtFile.responseText; 
+        //lines = txtFile.responseText.split("\n"); // Will separate each line into an array
+          var customTextElement = document.getElementById(IdInnerMDC);
+          customTextElement.innerHTML = txtFile.responseText;
+      }
+    }
+  }
+  txtFile.send(null);
+
+  document.getElementById(IdInnerMDC).innerHTML='<object type="text/html" data="' + option +'.html" ></object>';
 }
 
 function darkTheme(cb) {
-  var msg = new SpeechSynthesisUtterance('9 1 1 Call, A friend has been shoot at');
-  window.speechSynthesis.speak(msg);
-  msg = new SpeechSynthesisUtterance('Dispatch to 22 Lincon 18, responde code 3 to last niner');
-  window.speechSynthesis.speak(msg);
+  // var msg = new SpeechSynthesisUtterance('9 1 1 Call, A friend has been shoot at');
+  // window.speechSynthesis.speak(msg);
+  // msg = new SpeechSynthesisUtterance('Dispatch to 22 Lincon 18, responde code 3 to last niner');
+  // window.speechSynthesis.speak(msg);
     var mdc = document.getElementById(CompleteMDCid);
+    var leftMenu = document.getElementById(lefrMenuId);
     if (mdc){
         if(cb.checked){
             mdc.classList.remove(whiteThemeClass);
             mdc.classList.add(darkThemeClass);
+            leftMenu.classList.remove(whiteThemeClassMenu);
+            leftMenu.classList.add(darkThemeClassMenu);
             localStorage.setItem('darkTheme', true);
         }else{
             mdc.classList.add(whiteThemeClass);
             mdc.classList.remove(darkThemeClass);
+            leftMenu.classList.add(whiteThemeClassMenu);
+            leftMenu.classList.remove(darkThemeClassMenu);
             localStorage.setItem('darkTheme', false);
         }
     }
@@ -92,7 +122,21 @@ function dragElement(elmnt, clickElmnt) {
     }
   }
 
+  function showWanted(){
+    var audio = new Audio('sounds/wanted.mp3');
+    audio.volume = 0.2;
+    audio.play();
+    document.getElementById("wantedWarning").hidden = false;
+  }
 
+  function searchVehicle(){
+    document.getElementById(PersonContent).hidden = true;
+    document.getElementById(VehicleContent).hidden = false;
+  }
+  function searchPerson(){
+    document.getElementById(PersonContent).hidden = false;
+    document.getElementById(VehicleContent).hidden = true;
+  }
 
 
   ///////////////////////////////=================== JQUERRY
